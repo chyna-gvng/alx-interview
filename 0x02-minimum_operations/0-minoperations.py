e8526c787d57b9ad6510551745aaa4f1f74d6fe0
Walter <w.angoye@alustudent.com>
2023-07-12 20:44:34 +0300

    Updated 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,6 @@
     to result in exactly n H characters in the file.
 """
 
-
 def minOperations(n):
     """ 
         Calculates the fewest number of operations needed
@@ -20,13 +19,12 @@ def minOperations(n):
     summation = 1
     carrier = 0
 
-    while summation < n:
+    while summation != n:
         if n % summation == 0:
             carrier = summation
             summation *= 2
-            operations += 1
         else:
-            summation += carrier
+            summation += summation
         operations += 1
 
     return operations
