3f23590b687d6a8e17a3086cbcacdfe7237c4c0d
Walter <w.angoye@alustudent.com>
2023-07-19 18:19:39 +0300

    Updated 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
+""" reads stdin line by line and computes metrics """
 import sys
 
-def print_stats(total_size, status_counts):
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts):
-        print(f"{status_code}: {status_counts[status_code]}")
-
-def main():
-    total_size = 0
-    status_counts = {}
-
-    try:
-        for line_number, line in enumerate(sys.stdin, 1):
-            try:
-                _, _, _, _, _, status_code_str, file_size_str = line.split()
-                status_code = int(status_code_str)
-                file_size = int(file_size_str)
-            except (ValueError, IndexError):
-                continue
-
-            total_size += file_size
-            status_counts[status_code] = status_counts.get(status_code, 0) + 1
-
-            if line_number % 10 == 0:
-                print_stats(total_size, status_counts)
-
-    except KeyboardInterrupt:
-        # If a keyboard interrupt (CTRL + C) is detected, print the current stats.
-        print_stats(total_size, status_counts)
 
 if __name__ == "__main__":
-    main()
+    status = {"200": 0,
+              "301": 0,
+              "400": 0,
+              "401": 0,
+              "403": 0,
+              "404": 0,
+              "405": 0,
+              "500": 0}
+    count = 1
+    file_size = 0
+
+    def get_line(line):
+        """ parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in status.keys():
+                status[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats"""
+        print("File size: {}".format(file_size))
+        for key in sorted(status.keys()):
+            if status[key]:
+                print("{}: {}".format(key, status[key]))
+
+    try:
+        for line in sys.stdin:
+            file_size += get_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
