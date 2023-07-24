0c0e0c3dc112ab6a3a20cbedc4967df94c355bdb
Walter <w.angoye@alustudent.com>
2023-07-24 16:25:49 +0300

    Updated 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    python script that determines if a given data set represents a valid UTF-8 encoding
+    python script that determines if a given data set represents,
+    a valid UTF-8 encoding
 """
 
 
