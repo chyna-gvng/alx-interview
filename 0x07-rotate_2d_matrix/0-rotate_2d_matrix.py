1b8b4e5b476772de659c667236ffc4a373513d9c
Walter <w.angoye@alustudent.com>
2023-08-17 10:46:12 +0300

    Updated 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,11 @@
 def rotate_2d_matrix(matrix):
     """
-    Rotates a 2D matrix 90 degrees clockwise.
+        function that rotates a 2D matrix 90 degrees clockwise.
 
-    Args:
-        matrix: The 2D matrix to rotate.
+        Args:
+            matrix: The 2D matrix to rotate.
 
-    Returns:
-        None. The matrix is rotated in-place.
+        Return: Rotated matrix
     """
 
     n = len(matrix)
