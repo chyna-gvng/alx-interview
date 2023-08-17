883516e6d500632f88bb4e665545cd1298dfe6be
Walter <w.angoye@alustudent.com>
2023-08-17 10:48:13 +0300

    Updated 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,23 +1,11 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix """
+
+
 def rotate_2d_matrix(matrix):
-    """
-        function that rotates a 2D matrix 90 degrees clockwise.
+    """ Rotate 2D Matrix in-place """
+    matrix.reverse()
+    copy_matrix = matrix.copy()
 
-        Args:
-            matrix: The 2D matrix to rotate.
-
-        Return: Rotated matrix
-    """
-
-    n = len(matrix)
-
-    for layer in range(n // 2):
-        for i in range(layer, n - layer - 1):
-            top = matrix[layer][i]
-            bottom = matrix[n - layer - 1][i]
-            left = matrix[i][n - layer - 1]
-            right = matrix[i][layer]
-
-            matrix[layer][i] = left
-            matrix[n - layer - 1][i] = top
-            matrix[i][n - layer - 1] = right
-            matrix[i][layer] = bottom
+    for i in range(len(matrix)):
+        matrix[i] = [row[i] for row in copy_matrix]
